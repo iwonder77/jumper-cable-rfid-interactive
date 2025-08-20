fceca5f0492c3e9764f68d4b9eb1c36383741fce
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-08-20 16:35:01 -0600

    added rfid reader i2c address to initialization of terminal readers in constructor of battery object

--- Battery.h
+++ Battery.h
@@ -6,16 +6,16 @@
 
 #include "TerminalReader.h"
 
-// ----- TCA9548A Channels -----
+// ----- TCA9548A I2C MUX Channels -----
 const uint8_t NEGATIVE_TERMINAL_CHANNEL = 0;
 const uint8_t POSITIVE_TERMINAL_CHANNEL = 1;
 
 class Battery {
 public:
-  Battery(uint8_t muxAddr, uint8_t id)
+  Battery(uint8_t muxAddr, uint8_t id, uint8_t readerAddr)
       : muxAddr(muxAddr), id(id),
-        negative("Negative", NEGATIVE_TERMINAL_CHANNEL),
-        positive("Positive", POSITIVE_TERMINAL_CHANNEL) {}
+        negative(readerAddr, "Negative", NEGATIVE_TERMINAL_CHANNEL),
+        positive(readerAddr, "Positive", POSITIVE_TERMINAL_CHANNEL) {}
 
   void initializeReaders(MFRC522 &reader);
   void updateReaders(MFRC522 &reader);
@@ -24,6 +24,7 @@ public:
 
   const TerminalReader &getPositive() const { return positive; }
   const TerminalReader &getNegative() const { return negative; }
+  uint8_t getMuxAddr() const { return muxAddr; }
   uint8_t getId() const { return id; }
 
 private:
