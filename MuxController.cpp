7ff8bd7e63ce9568e9a3575369e65e4dc08fe2bd
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-08-21 10:45:29 -0600

    removed channel disabling before channel switching in selectChannel() method, not necessary

--- MuxController.cpp
+++ MuxController.cpp
@@ -2,11 +2,6 @@
 #include "Arduino.h"
 
 void MuxController::selectChannel(uint8_t muxAddress, uint8_t channel) {
-  // disable channel first
-  Wire.beginTransmission(muxAddress);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  delay(5);
   if (channel > 7)
     return;
   Wire.beginTransmission(muxAddress);
