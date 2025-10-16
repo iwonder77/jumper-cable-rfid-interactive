a0a574c6bb95008b26db58768b737751214ea87e
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-10-16 11:26:10 -0600

    I thought I already said this but this file now works for special 6V, 12V, 16V aniamtions and has debugging output

--- rp2040/rp2040.ino
+++ rp2040/rp2040.ino
@@ -4,31 +4,34 @@
 
 LEDController ledController;
 
-volatile uint8_t currentAnimation = config::CMD_DEFAULT_ANIMATION;
+volatile uint8_t currentAnimation = 0xFF;
 volatile bool gotNewCommand = false;
-uint8_t animation = config::CMD_DEFAULT_ANIMATION;
+uint8_t animation = 0xFF;
+uint8_t lastPlayedAnimation = 0xFF;  // Track last animation to detect changes
 unsigned long animationEndTimestamp = 0;
 
 void receiveEvent(int numBytes) {
-  if (Wire.available()) {
-    uint8_t cmd = Wire.read();
-    if (cmd == config::CMD_6V_ANIMATION || cmd == config::CMD_12V_ANIMATION || cmd == config::CMD_16V_ANIMATION) {
-      currentAnimation = cmd;
-      gotNewCommand = true;
-    } else if (cmd == config::CMD_DEFAULT_ANIMATION) {
-      currentAnimation = cmd;
-    }
-    while (Wire.available()) {
-      Wire.read();
-    }
+  if (numBytes <= 0) return;
+  uint8_t cmd = Wire.read();
+  if (cmd == config::CMD_6V_ANIMATION || cmd == config::CMD_12V_ANIMATION || cmd == config::CMD_16V_ANIMATION) {
+    currentAnimation = cmd;
+    gotNewCommand = true;
+  } else if (cmd == config::CMD_DEFAULT_ANIMATION) {
+    currentAnimation = cmd;
+  }
+  while (Wire.available()) {
+    Wire.read();
   }
 }
 
 void setup() {
+  Serial.begin(115200);
+  delay(50);
+  Serial.println("RP2040 booting...");
+
   Wire.begin(config::LED_CONTROLLER_ADDR);
   Wire.onReceive(receiveEvent);
 
-  Serial.begin(115200);
   ledController.initialize();
   Serial.println("LEDController initialized");
 }
@@ -44,17 +47,42 @@ void loop() {
   }
 
   // determine which animation to play (default or 6V,12V,16V for specified amnt of time)
+  uint8_t activeAnimation;
   if (animationEndTimestamp != 0 && millis() < animationEndTimestamp) {
     // special animation timer is active and has not expired yet
-    ledController.update(animation);
-    Serial.println("special anim");
+    activeAnimation = animation;
   } else {
     // timer expired or no special animation - play default animation
     if (animationEndTimestamp != 0) {
       animationEndTimestamp = 0;
     }
-    ledController.update(config::CMD_DEFAULT_ANIMATION);
-    Serial.println("default anim");
+    activeAnimation = config::CMD_DEFAULT_ANIMATION;
   }
-  Serial.println("man what");
+
+  // Print debug message only when animation changes
+  if (activeAnimation != lastPlayedAnimation) {
+    Serial.print("Animation changed to: ");
+    switch (activeAnimation) {
+      case config::CMD_DEFAULT_ANIMATION:
+        Serial.println("DEFAULT");
+        break;
+      case config::CMD_6V_ANIMATION:
+        Serial.println("6V");
+        break;
+      case config::CMD_12V_ANIMATION:
+        Serial.println("12V");
+        break;
+      case config::CMD_16V_ANIMATION:
+        Serial.println("16V");
+        break;
+      default:
+        Serial.print("UNKNOWN (");
+        Serial.print(activeAnimation);
+        Serial.println(")");
+        break;
+    }
+    lastPlayedAnimation = activeAnimation;
+  }
+
+  ledController.update(activeAnimation);
 }
