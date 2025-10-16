1fd9fc1b7444a3315811ad71ab5ae35745cb930a
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2025-10-16 11:23:01 -0600

    debugging output to ensure commands were being sent correctly using wire.endTransmission result

--- mkrzero-rx/LEDCommander.cpp
+++ mkrzero-rx/LEDCommander.cpp
@@ -1,4 +1,5 @@
 #include "LEDCommander.h"
+#include "Debug.h"
 #include <Arduino.h>
 #include <FastLED.h>
 
@@ -6,6 +7,8 @@ void LEDCommander::init() { sendCommand(config::CMD_DEFAULT_ANIMATION); }
 
 bool LEDCommander::sendCommand(uint8_t cmd, uint8_t *params,
                                uint8_t paramCount) {
+  DEBUG_PRINTLN("Sending LED command to RP2040");
+
   Wire.beginTransmission(config::LED_CONTROLLER_ADDR);
   Wire.write(cmd);
 
@@ -15,10 +18,27 @@ bool LEDCommander::sendCommand(uint8_t cmd, uint8_t *params,
 
   uint8_t error = Wire.endTransmission();
 
-  if (error != 0) {
-    Serial.print("I2C Error: ");
-    Serial.println(error);
-    return false;
+  DEBUG_PRINTLN("I²C result: ");
+  switch (error) {
+  case 0:
+    DEBUG_PRINTLN("SUCCESS");
+    return true;
+  case 1:
+    DEBUG_PRINTLN("ERROR: Data too long");
+    break;
+  case 2:
+    DEBUG_PRINTLN("ERROR: NACK on address"); // ← Most likely issue
+    break;
+  case 3:
+    DEBUG_PRINTLN("ERROR: NACK on data");
+    break;
+  case 4:
+    DEBUG_PRINTLN("ERROR: Other");
+    break;
+  default:
+    DEBUG_PRINT("ERROR: Unknown (");
+    DEBUG_PRINT(error);
+    DEBUG_PRINTLN(")");
   }
-  return true;
+  return false;
 }
